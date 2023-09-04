f02a2fd714e5ae3df6d7b40040eee9c5d61e14b6
Re-he4i5 <kuronekoryohei@gmail.com>
2023-09-04 22:35:56 +0900

    react-router-domのインストール、ログイン/アウトのコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
-import { BrouserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import CreatePost from "./components/CreatePost";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
