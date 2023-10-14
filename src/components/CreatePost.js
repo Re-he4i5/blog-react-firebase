3846d49dedca6521206ddba1b32bebcbc53348a4
Re-he4i5 <kuronekoryohei@gmail.com>
2023-10-14 14:08:11 +0900

    投稿ページのレイアウト

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function CreatePost() {
-  return <div>CreatePost</div>;
+  return (
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className="inputPost">
+          <input type="text" placeholder="タイトル" />
+        </div>
+        <div className="inputPost">
+          <textarea placeholder="本文" />
+        </div>
+        <div className="inputPost">
+          <button className="postButton">投稿</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreatePost;
