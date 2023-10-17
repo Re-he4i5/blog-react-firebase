2878419076c8e4e777b728905d6cb4618db92a01
recodeyo1ko <kuronekoryohei@gmail.com>
2023-10-17 18:47:44 +0900

    投稿機能

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,13 @@ const Navbar = ({ isAuth }) => {
   return (
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/createpost">Create Post</Link>
       {!isAuth ? (
         <Link to="/login">Login</Link>
       ) : (
-        <Link to="/logout">Logout</Link>
+        <>
+          <Link to="/createpost">Create Post</Link>
+          <Link to="/logout">Logout</Link>
+        </>
       )}
     </nav>
   );
